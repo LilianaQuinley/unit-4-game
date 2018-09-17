abf3062c679eee30d1b526d30d04bfb41dbd34d2
liliana Quinley <lilicuellar99@gmail.com>
2018-09-16 23:57:51 -0600

    pre-final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,7 @@
 
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
 
 $( document ).ready(function() {
     console.log( "ready!" );
@@ -22,32 +25,67 @@ $( document ).ready(function() {
     var RubyRandomNumber = randomNumberFromRange(1, 12);
     var SaphireRandomNumber = randomNumberFromRange(1, 12);
 
+/// CREATED A FUNCTION TO ADD THE SCORE FOR EACH CRYSTAL SELECTED
+
+    function AddingtotalScore(ChrystalChoice) {
+        totalScore = totalScore + ChrystalChoice;     
+         console.log(totalScore);
+        //// DISPLAY  THE TOTALSCORE  ON TOTAL SCORE AREA
+        $("#addTotal").text(totalScore);     
+        
+
+    // WINN /LOSS 
+
+        if (totalScore === randomNumber) {
+            wins++;
+            $("#AlertwinnLoss").html("<p>you win!</p>");
+            $("#wins").html(wins);
+            resetGame();
+        }
+
+        else if (totalScore > randomNumber) {
+            losses++;
+            $("#AlertwinnLoss").html("<p>you lost!</p>");
+            $("#losses").html(losses);
+            resetGame();
+        }
+    }
+
+    function resetGame() {
+            randomNumber = randomNumberFromRange(19, 120);
+            diamondRandomNumber = randomNumberFromRange(1, 12);
+            emeraldRandomNumber = randomNumberFromRange(1, 12);
+            RubyRandomNumber = randomNumberFromRange(1, 12);
+            SaphireRandomNumber = randomNumberFromRange(1, 12);
+            totalScore = 0;   
+
+             // updated HTML page
+            $("#newGame").text(randomNumber); 
+            $("#addTotal").text("0"); 
+        }
+
 /// ONCLICK Give the each gem a random number  
 
     $(".diamond").on("click", function onGemsClick(){
-        $(".diamond").text(diamondRandomNumber);  
+       AddingtotalScore(diamondRandomNumber);  
         console.log(this)
     });
     $(".emerald").on("click", function onGemsClick(){
-        $(".emerald").text(emeraldRandomNumber);  
+        AddingtotalScore(emeraldRandomNumber);  
         console.log(this)
     });
     $(".ruby").on("click", function onGemsClick(){
-        $(".ruby").text(RubyRandomNumber);  
+        AddingtotalScore(RubyRandomNumber)  
         console.log(this)
     });
     $(".saphire").on("click", function onGemsClick(){
-        $(".saphire").text(SaphireRandomNumber);  
+        AddingtotalScore(SaphireRandomNumber) 
         console.log(this)
     });
-
 
 
 });
 
 
-
-
-
-
-
+  
+  
